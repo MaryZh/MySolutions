65dce9d5bd571209833af03fe47b5904750bac2d
MaryZh <guanghua@iastate.edu>
2017-07-14 17:54:32 -0500

    Update solution.java

--- Backtracking/77. Combinations/solution.java
+++ Backtracking/77. Combinations/solution.java
@@ -2,26 +2,19 @@
 
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        int[] nums = new int[n];
-        for(int i=0; i<n; i++)
-            nums[i]=i+1;
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(nums, res, new ArrayList<>(), k, 0, new boolean[nums.length]);
+        backtrack(res, new ArrayList<>(), k, n, 1);
         
         return res;
     }
-    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> templist, int k, int start, boolean[] used){
-        if(templist.size() ==k)
+    private void backtrack(List<List<Integer>> res, List<Integer> templist, int k, int n, int start){
+        if(k == 0)
             res.add(new ArrayList(templist));
-        else if(templist.size() < k){
-            for(int i=start; i<nums.length; i++){
-                if(used[i]) 
-                    continue;
-                templist.add(nums[i]);
-                used[i]=true;
-                backtrack(nums, res, templist,k,i+1, used);
+        else if(k >0){
+            for(int i=start; i<n+1; i++){
+                templist.add(i);
+                backtrack(res, templist,k-1, n, i+1);
                 templist.remove(templist.size()-1);
-                used[i]=false;
             }
         }
     }
