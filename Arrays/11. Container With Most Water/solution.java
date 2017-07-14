0ea6581f5565647aa1d138c57b8a042786079317
MaryZh <guanghua@iastate.edu>
2017-07-13 22:20:07 -0500

    Update solution.java

--- Arrays/11. Container With Most Water/solution.java
+++ Arrays/11. Container With Most Water/solution.java
@@ -1,6 +1,7 @@
 // This problem is to find the maxArea of a square: with width abs(j-i), and height min(A[i], A[j])
 // brute force method. Time complexity O(N^2). TLE.
-// This is two-point method: one pointer at the beginning, the other at the end. If (left pointer length)<(right pointer legnth), the height // of the container is limited by (left pointer length), decreasing right pointer is not possible to increase the area; increasing 
+// This is two-point method: one pointer at the beginning, the other at the end. If (left pointer length)<(right pointer legnth), the height 
+// of the container is limited by (left pointer length), decreasing right pointer is not possible to increase the area; increasing 
 // left pointeris potentially beneficial, so increasing left pointer. Update maxarea for each iteration.
 //
 // Time complexity: O(N). Space complexity: O(1).
