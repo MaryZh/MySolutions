c95be598de315a376eea4a1ed4c926867ad4d408
MaryZh <guanghua@iastate.edu>
2017-07-13 21:37:57 -0500

    Create solution1.java

---
+++ Arrays/1. Two Sum/solution1.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // use Hash map to pair the (value, index) together
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i< nums.length; i++){
+        	if(map.containsKey(target-nums[i])){
+        		return new int[] {map.get(target-nums[i]),i};
+        	}
+        	map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No solution! ");
+    }
+}
