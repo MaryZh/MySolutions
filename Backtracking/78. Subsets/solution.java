e96ee17ff04593059ffeeffb0d88e06fe14806a2
MaryZh <guanghua@iastate.edu>
2017-07-14 17:49:33 -0500

    Update solution.java

--- Backtracking/78. Subsets/solution.java
+++ Backtracking/78. Subsets/solution.java
@@ -1,23 +1,17 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backtrack(nums, res, new ArrayList<>(), 0, new boolean[nums.length]);
+        backtrack(nums, res, new ArrayList<>(), 0);
         
         return res;
     }
-    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> templist, int start, boolean[] used){
+    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> templist, int start){
         res.add(new ArrayList(templist));
         
         for(int i=start; i<nums.length; i++){
-            if(used[i]) 
-                continue;
-            // if(templist.contains(nums[i]) )  // either use this or boolean array used to determine if an elment in nums has been used or not
-            //    continue;
             templist.add(nums[i]);
-            used[i]=true;
             backtrack(nums, res, templist,i+1, used);
             templist.remove(templist.size()-1);
-            used[i]=false;
         }
         
     }
