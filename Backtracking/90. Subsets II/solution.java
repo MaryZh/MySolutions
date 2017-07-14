3509a8d9553f4e1988615fd8b260344a1dc6ecfc
MaryZh <guanghua@iastate.edu>
2017-07-14 17:50:48 -0500

    Update solution.java

--- Backtracking/90. Subsets II/solution.java
+++ Backtracking/90. Subsets II/solution.java
@@ -2,24 +2,22 @@ public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        backtrack(nums, res, new ArrayList<>(), 0, new boolean[nums.length]);
+        backtrack(nums, res, new ArrayList<>(), 0);
         
         return res;
     }
-    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> templist, int start, boolean[] used){
+    private void backtrack(int[] nums, List<List<Integer>> res, List<Integer> templist, int start){
         // ArrayList<Integer> cur = new ArrayList(templist);
         // if(! res.contains(cur))
         //     res.add(cur);
         res.add(new ArrayList(templist));
         
         for(int i=start; i<nums.length; i++){
-            if(used[i] || (i>0 && nums[i]==nums[i-1] && !used[i-1])) 
+            if(i>0 && nums[i]==nums[i-1]) 
                 continue;
             templist.add(nums[i]);
-            used[i]=true;
-            backtrack(nums, res, templist,i+1, used);
+            backtrack(nums, res, templist,i+1);
             templist.remove(templist.size()-1);
-            used[i]=false;
         }
     }
 }
