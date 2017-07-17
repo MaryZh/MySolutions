208b8471193ac55acb04eb132b491e165d5ddbe9
MaryZh <guanghua@iastate.edu>
2017-07-16 19:10:49 -0500

    Update solution1.java

--- Arrays/120. Triangle/solution1.java
+++ Arrays/120. Triangle/solution1.java
@@ -1,44 +1,20 @@
-// top-down solution. slow.
-// Use two HashMap/ArrayList's to store minPath for cur and next row.
+// top-down solution. 7ms (85.71%).
+// Use two int[]'s to store minPath for cur and next row. (Note: use ArrayList/HashMap would be mush slower.)
 
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         // min path sum for current row (index, minpath)
-        HashMap<Integer, Integer> res =  new HashMap<>();
+        int[] res =  new int[1];
         // initialize for 0th row
-        res.put(0,triangle.get(0).get(0));
+        res[0]=triangle.get(0).get(0);
         
         for(int i =1; i<triangle.size(); i++){
             // current row
             List<Integer> cur = triangle.get(i);
-            HashMap<Integer, Integer> newres =  new HashMap<>();
-            for(int j=0; j<cur.size(); j++){
-                newres.put(j, Math.min(res.getOrDefault(j-1,Integer.MAX_VALUE), res.getOrDefault(j,Integer.MAX_VALUE) ) + cur.get(j) );
-            }
-            res = newres;
-        }
-        int minPath = Integer.MAX_VALUE;
-        for(int k:res.values()){
-            minPath = Math.min(minPath, k);
-        }
-        return minPath;
-    }
-}
-
-// Use ArrayList
-public class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        // min path sum for current row (index, minpath)
-        ArrayList<Integer> res =  new ArrayList<>();
-        // initialize for 0th row
-        res.add(triangle.get(0).get(0));
-        
-        for(int i =1; i<triangle.size(); i++){
-            // current row
-            List<Integer> cur = triangle.get(i);
-            ArrayList<Integer> newres =  new ArrayList<>();
-            for(int j=0; j<cur.size(); j++){
-                newres.add(Math.min( j-1>=0?res.get(j-1):Integer.MAX_VALUE, j<res.size()?res.get(j):Integer.MAX_VALUE ) + cur.get(j) );
+            int n=cur.size();
+            int[] newres =  new int[n];
+            for(int j=0; j<n; j++){
+                newres[j]=Math.min( j-1>=0?res[j-1]:Integer.MAX_VALUE, j<i?res[j]:Integer.MAX_VALUE ) + cur.get(j) ;
             }
             res = newres;
         }
