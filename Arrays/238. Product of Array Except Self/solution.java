63242145f90a11d0356ee242468848e9cf881a65
MaryZh <guanghua@iastate.edu>
2017-07-17 12:11:56 -0500

    Create solution.java

---
+++ Arrays/238. Product of Array Except Self/solution.java
@@ -0,0 +1,22 @@
+// This method is from 
+// https://stackoverflow.com/questions/2680548/given-an-array-of-numbers-return-array-of-products-of-all-other-numbers-no-div .
+
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        
+        res[0]=1;
+        for(int i=1; i<n; i++){
+            res[i] = res[i-1]*nums[i-1];
+        }
+        
+        int right =1;
+        for(int i=n-1; i>=0; i--){
+            res[i] *=right;
+            right *=nums[i];
+        }
+        
+        return res;
+    }
+}
