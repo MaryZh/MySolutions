062f54a5ca876ea41493e934e990e0cc680fb975
MaryZh <guanghua@iastate.edu>
2017-07-18 11:29:56 -0500

    Update solution1.java

--- Linked List/206. Reverse Linked List/solution1.java
+++ Linked List/206. Reverse Linked List/solution1.java
@@ -8,14 +8,15 @@
  */
 public class Solution {
     public ListNode reverseList(ListNode head) {
-        // recursively
-        return reverseTwoNodes(head,null);
-    }
-    private ListNode reverseTwoNodes(ListNode head, ListNode pre){
-        if(head == null)
-            return pre;
-        ListNode temp = head.next;
-        head.next=pre;
-        return reverseTwoNodes(temp,head);
+        if( head == null || head.next == null )
+            return head;
+        ListNode pre = null, cur=head;
+        while(cur != null){
+            ListNode temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
     }
 }
