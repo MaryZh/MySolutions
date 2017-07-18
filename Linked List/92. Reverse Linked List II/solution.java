6dd35a45c53445ec1b867bbf6ebe44ec1c484f29
MaryZh <guanghua@iastate.edu>
2017-07-18 12:50:48 -0500

    Update solution.java

--- Linked List/92. Reverse Linked List II/solution.java
+++ Linked List/92. Reverse Linked List II/solution.java
@@ -16,12 +16,12 @@ public class Solution {
             pre = pre.next;
         }
         // gradually reverse the nodes along the way
-        ListNode start = pre.next, then =start.next;
+        ListNode start = pre.next;
         for(int i=1; i<= n-m; i++ ){
+            ListNode then = start.next;
             start.next = then.next;
             then.next = pre.next;
             pre.next = then;
-            then = start.next;
         }
         
         return dummy.next;
@@ -29,14 +29,12 @@ public class Solution {
 }
 
 // Eg. d -> 1- 2-3-4-5-null. m=2, n=4. 
-// First reversing, initialization: d -> 1(pre)- 2(start)-3(then)-4-5-null.
+// First reversing, initialize (then): d -> 1(pre)- 2(start)-3(then)-4-5-null.
 // 1. start.next = then.next; d -> 1(pre)- 2(start)-4-5-null, 3(then)-4-5-null
 // 2. then.next = pre.next; d -> 1(pre)- 2(start)-4-5-null, 3(then)- 2(start)-4-5-null
 // 3. pre.next = then; d -> 1(pre)-3(then)- 2(start)-4-5-null
-// 4. then = start.next; d -> 1(pre)-3- 2(start)-4(then)-5-null
 //
-// Second reversing: initialization: d -> 1(pre)-3- 2(start)-4(then)-5-null
+// Second reversing: initialize (then): d -> 1(pre)-3- 2(start)-4(then)-5-null
 // 1. start.next = then.next; d -> 1(pre)-3- 2(start)-5-null, 4(then)-5-null
 // 2. then.next = pre.next; d -> 1(pre)-3- 2(start)-5-null, 4(then)-3- 2(start)-5-null
 // 3. pre.next = then; d -> 1(pre)-4(then)-3- 2(start)-5-null
-// 4. then = start.next; d -> 1(pre)-4-3- 2(start)-5(then)-null
