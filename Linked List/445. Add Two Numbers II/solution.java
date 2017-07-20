fdb667d9fb55b1fd3ad60cc232b9b53edb43efd7
MaryZh <guanghua@iastate.edu>
2017-07-20 17:34:20 -0500

    Create solution.java

---
+++ Linked List/445. Add Two Numbers II/solution.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // use a stack to store the values in each list
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while(l1 != null){
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while(l2 != null){
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        // add number in stack2 to stack1
+        ListNode cur = null, head= null;
+        int carry =0;
+        while( !stack1.isEmpty() || !stack2.isEmpty() ){
+            int sum =carry;
+            if(!stack1.isEmpty()){
+                sum += stack1.pop();
+            }
+            if(!stack2.isEmpty()){
+                sum += stack2.pop();
+            }
+            head = new ListNode(sum%10);
+            head.next = cur;
+            carry = sum/10;
+            cur = head;
+        }
+        
+        if(carry == 1){
+            head = new ListNode(carry);
+            head.next = cur;
+        }
+        return head;
+    }
+}
