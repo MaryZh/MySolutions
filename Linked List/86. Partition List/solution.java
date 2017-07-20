3f74c1adcd4422a64a995f7788ed07c5f97a1341
MaryZh <guanghua@iastate.edu>
2017-07-20 15:30:01 -0500

    Update solution.java

--- Linked List/86. Partition List/solution.java
+++ Linked List/86. Partition List/solution.java
@@ -11,20 +11,19 @@
 public class Solution {
     public ListNode partition(ListNode head, int x) {
         // create a dummy node; iterate through the list, if less than x, remove it and append it to dummy
-        ListNode smallerHead =  new ListNode(0), largerHead =  new ListNode(0);
+        ListNode smallerHead = new ListNode(0), largerHead = new ListNode(0);
         ListNode smaller = smallerHead, larger = largerHead;
         while(head != null){
             if(head.val < x){
-                smaller.next = head;
-                smaller = head;
+                smaller = smaller.next = head;
             }
             else{
-                larger.next =head;
-                larger = head;
+                larger = larger.next =head;
             }
             head = head.next;
         }
-        smaller.next = larger.next;
+        larger.next = null;
+        smaller.next = largerHead.next;
         return smallerHead.next;
     }
 }
