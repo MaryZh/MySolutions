cf0f270a8b8084d1792be88c2581fe6c529b6a34
MaryZh <guanghua@iastate.edu>
2017-07-20 20:07:53 -0500

    Create solution2.java

---
+++ Divide and conquer/215. Kth Largest Element in an Array/solution2.java
@@ -0,0 +1,13 @@
+// Use a priorty queue. O(n log k)
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q= new PriorityQueue<>();
+        for(int n:nums){
+            q.offer(n);
+            if(q.size()>k)
+                q.poll();
+        }
+        return q.poll();
+    }
+}
