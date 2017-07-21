c4affb72a8d6786fcd7dab2125f8a873288492d3
MaryZh <guanghua@iastate.edu>
2017-07-20 20:59:06 -0500

    Create solution.java

---
+++ Divide and conquer/240. Search a 2D Matrix II/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        if(m==0 )
+            return false;
+        int n = matrix[0].length;
+        if(n ==0)
+            return false;
+        // start search from the top right corner
+        int row = 0, col = n-1;
+        while(row< m && col>=0){
+            if(matrix[row][col] == target)
+                return true;
+            else if(matrix[row][col] < target)
+                row++;
+            else
+                col--;
+        }
+        return false;
+    }
+}
