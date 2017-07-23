e09af1faadf7e105dcc4782aecfffc9b4bcf4f7e
MaryZh <guanghua@iastate.edu>
2017-07-23 14:48:41 -0500

    Update solution.java

--- Backtracking/37. Sudoku Solver/solution.java
+++ Backtracking/37. Sudoku Solver/solution.java
@@ -1,21 +1,22 @@
 public class Solution {
     public void solveSudoku(char[][] board) {
-        int n = board.length;
-        solve(board, n);
+        // sudoku's dimension is 9 for sure, so no need to calculate the dimension of board
+        // int n = board.length;
+        solve(board);
     }
     // check if the board is solvable
-    private boolean solve(char[][] board, int n){
+    private boolean solve(char[][] board){
         // iterate trough each site
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
                 // for each empty site, try 1-9
                 if(board[i][j] =='.'){
                     for(char c = '1'; c <= '9'; c++){
-                        if(isValid(board, n, i, j, c)){
+                        if(isValid(board, i, j, c)){
                             // System.out.println("(i, j) = ("+i+", "+j+"), c valid: " + c);
                             board[i][j] = c;
                     
-                            if(solve(board, n) )    // if this soln return true
+                            if(solve(board) )    // if this soln return true
                                 return true;
                             
                             board[i][j] = '.';   // otherwise go back
@@ -29,17 +30,15 @@ public class Solution {
         return true;
     }
     // check if c is valid fill for (row,col)
-    private boolean isValid(char[][] board, int n, int row, int col, char c){
-        for(int i=0; i< n; i++){
+    private boolean isValid(char[][] board, int row, int col, char c){
+        for(int i=0; i< 9; i++){
             // check row
             if(board[row][i] == c)
                 return false;
             // check column
             if(board[i][col] == c)
                 return false; 
-        }
-        // check 3x3 cells; left-top cell: ((row/3)*3, (col/3)*3)
-        for(int i=0; i<9; i++){
+            // check 3x3 sub-boxes; left-top cell: ((row/3)*3, (col/3)*3)
             if(board[i/3+(row/3)*3][i%3+(col/3)*3] == c)
                 return false;
         }
