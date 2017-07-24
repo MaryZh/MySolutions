703bac88b8fea89ff44fbaaaa9b4ca578638f3d0
MaryZh <guanghua@iastate.edu>
2017-07-24 10:13:48 -0500

    Create mysolution.java

---
+++ Dynamical programming/357. Count Numbers with Unique Digits/mysolution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n==0)
+            return 1;
+        int[] dp = new int[n+1];
+        dp[0]=1;
+        dp[1]=10;
+        for(int i=2; i<=n; i++){
+            // dp[i] = dp[i-1] + (dp[i-1]-dp[i-2])*(10-(i-1));
+            dp[i] = dp[i-1] + (dp[i-1]-dp[i-2])*(11-i);
+        }
+        return dp[n];
+    }
+}
