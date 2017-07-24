8a4e0fbe97e71cf95304ff5a0dc0777e85976605
MaryZh <guanghua@iastate.edu>
2017-07-23 19:34:07 -0500

    Create solution.java

---
+++ Dynamical programming/338. Counting Bits/solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int[] countBits(int num) {
+        int[] dp = new int[num+1];
+        // dp[0]=0; // not needed since initialized as 0
+        for(int i=1; i<=num; i++){
+            dp[i]=dp[i >>1] + (i&1);
+        }
+        return dp;
+    }
+}
