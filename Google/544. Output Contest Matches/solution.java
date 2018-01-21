606ebfce273694d6bb242cab9d329b5d49599d31
MaryZh <guanghua@iastate.edu>
2018-01-21 16:07:28 -0600

    Create solution.java

---
+++ Google/544. Output Contest Matches/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public String findContestMatch(int n) {
+        String[] team = new String[n];
+        for (int i = 0; i < n; i++) {
+            team[i] = (i + 1)+"";
+        }
+
+        while (n > 1) {
+            for (int i = 0; i < n / 2; i++) {
+                team[i] = "(" + team[i] + "," + team[n - 1 - i] + ")";
+            }
+            n /= 2;
+        }
+        
+        return team[0];
+    }
+}
