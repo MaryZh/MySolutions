0fbaecedc18c93a909113ca4f2833d65a2e997b7
MaryZh <guanghua@iastate.edu>
2018-01-30 10:06:42 -0600

    Create solution.java

---
+++ Google/276. Paint Fence/solution.java
@@ -0,0 +1,13 @@
+public int numWays(int n, int k) {
+    if(n == 0) return 0;
+    else if(n == 1) return k;
+    // for n=2
+    int diffColorCounts = k*(k-1);
+    int sameColorCounts = k;
+    for(int i=3; i<=n; i++) {
+        int temp = diffColorCounts;
+        diffColorCounts = (diffColorCounts + sameColorCounts) * (k-1);
+        sameColorCounts = temp;
+    }
+    return diffColorCounts + sameColorCounts;
+}
